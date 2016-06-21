f2fc2cb4bcf740135927f7650206e4dffb9c6297
Konrad Oleksiuk <konole@gmail.com>
2016-06-21 10:29:26 +0200

    Add build.sh

---
+++ elasticsearch/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ "$1" == "dev" ]; then
+    HTTP_PORT="9201";
+    SERVICE_PORT="9301";
+elif [ "$1" == "test" ]; then
+    HTTP_PORT="9200";
+    SERVICE_PORT="9300";
+else
+    echo "Uknown environment $1 - [test, dev] available";
+    exit 1;
+fi
+
+docker run --name=elasticsearch-$1 -p $HTTP_PORT:9200 -p $SERVICE_PORT:9300 -d konole/elasticsearch-lf
